5891b6d20634d3b9e884ae3471e72c1af42e3998
dsuttonpreece <danielsuttonp@gmail.com>
2023-01-15 22:32:35 +1100

    test(timeline) insert new timeline event

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test } from "@jest/globals";
+import { afterAll, beforeAll, expect, test } from "@jest/globals";
 
 import { DockerComposeEnvironment } from "testcontainers";
 import { Client } from "pg";
@@ -38,5 +38,34 @@ test("spin up postgres container", () => {
 test("connect to postgres container", async () => {
   const client = new Client(options);
   await client.connect();
+
+  client.end();
+});
+
+test.only("create events table", async () => {
+  const client = new Client(options);
+  await client.connect();
+
+  await client.query(`
+    CREATE TABLE events (
+      id SERIAL,
+      date_created timestamptz,
+      date_updated timestamptz,
+      type VARCHAR, -- TODO: move to enum table
+      properties jsonb
+    );
+  `);
+
+  try {
+    const { rows } = await client.query(`
+    INSERT INTO events (type, properties) VALUES 
+      ('create_entity', '{"uuid": "asdf1234"}')
+    RETURNING id;
+  `);
+    expect(rows).toEqual([{ id: 1 }]);
+  } catch (error) {
+    console.error("@@ Insert event error", error);
+  }
+
   client.end();
 });
