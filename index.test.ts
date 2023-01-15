d440781a69c214ea50d2038b18c07f8b7cbcc86e
dsuttonpreece <danielsuttonp@gmail.com>
2023-01-15 20:36:01 +1100

    add gitignore

---
+++ index.test.ts
@@ -0,0 +1,32 @@
+import { afterAll, beforeAll, test } from "@jest/globals";
+
+import { DockerComposeEnvironment } from "testcontainers";
+
+let environment;
+let container;
+
+beforeAll(async () => {
+  const composeFilePath = __dirname;
+  const composeFile = "docker-compose.yml";
+
+  const options = {
+    user: "test",
+    password: "test",
+    database: "test",
+  };
+
+  environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
+    .withEnvironment(options)
+    .up();
+
+  container = environment.getContainer("postgres-1");
+});
+
+afterAll(async () => {
+  await container.stop();
+  await environment.down();
+});
+
+test("spin up postgres container", async () => {
+  expect(true).toBe(true);
+});
