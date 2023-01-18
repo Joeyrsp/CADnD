6ce2d907d0a9792196c30d490333708b5a576275
dsuttonpreece <danielsuttonp@gmail.com>
2023-01-18 14:04:03 +1100

    feat(timeline) tidy activateEffect function

--- activateEffect.ts
+++ activateEffect.ts
@@ -6,18 +6,25 @@ export const activateEffect = async (
 ) => {
   const { event_type: eventType, properties: eventProps, id: eventId } = event;
   const { component_uuid } = eventProps;
-  const { slots } = components[component_uuid];
+
+  const effectComponent = components[component_uuid];
+  const { slots } = effectComponent;
 
   if (slots) {
-    for (const slot of slots) {
-      if (event.properties[slot.name]) {
-        console.log(slot);
+    slots.forEach(slot => {
+      const { name: slotName, slot_type: slotType } = slot;
+
+      if (eventProps[slotName]) {
+        if (slotType === "roll") {
+          // TODO: parse roll def string
+          console.log("Roll: ", Math.random());
+        }
       } else {
         console.error(
-          `Event '${eventId}' of type '${eventType}' must have key '${slot.name}' to match it's source effect component slot.`
+          `Event '${eventId}' of type '${eventType}' must have key '${slotName}' to match it's source effect component slot.`
         );
       }
-    }
+    });
   } else {
     console.error(`Events of type '${eventType}' must have 'slots' prop.`);
   }
