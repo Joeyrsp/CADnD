5182795dd60d93eb45863aee0349d000d2f48dcd
dsuttonpreece <danielsuttonp@gmail.com>
2023-01-18 13:37:56 +1100

    feat(timeline) add basic slot validation on activate_effect event

--- timeline.ts
+++ timeline.ts
@@ -1,4 +1,25 @@
-export interface Event {
+export interface Component {
+  [key: string]: any;
+}
+
+export interface EventPropsActivateEffect {
+  component_uuid: string;
+  source_uuid: string;
+  target_uuid: string;
+  [key: string]: any;
+}
+
+export interface EventPropsComponent {
+  component_uuid: string;
+  entity_uuid: string;
+  component: Component;
+}
+
+export interface EventPropsEntity {
+  entity_uuid: string;
+}
+
+export interface EventBasic {
   date_created: string | null;
   date_updated: string | null;
   event_type: string;
@@ -7,15 +28,33 @@ export interface Event {
   properties: any;
 }
 
+export interface EventCreateEntity extends EventBasic {
+  event_type: "create_entity";
+  properties: EventPropsEntity;
+}
+
+export interface EventCreateComponent extends EventBasic {
+  event_type: "create_component";
+  properties: EventPropsComponent;
+}
+
+export interface EventActivateEffect extends EventBasic {
+  event_type: "activate_effect";
+  properties: EventPropsActivateEffect;
+}
+
+export type Event =
+  | EventCreateEntity
+  | EventCreateComponent
+  | EventActivateEffect;
+
 export type Timeline = Event[];
 
 export type Entity = String;
 
 export const runTimeline = (timeline: Timeline) => {
   const entities: Entity[] = [];
-  const components: { [key: string]: Event } = {};
-
-  let currentComponent;
+  const components: { [key: string]: Component } = {};
 
   for (const event of timeline) {
     switch (event.event_type) {
@@ -30,11 +69,24 @@ export const runTimeline = (timeline: Timeline) => {
 
       case "activate_effect":
         const { component_uuid } = event.properties;
-        const sourceComponent = components[component_uuid];
-        console.log(
-          "@@ ~ file: timeline.ts:34 ~ runTimeline ~ sourceComponent",
-          sourceComponent
-        );
+        const { slots } = components[component_uuid];
+
+        if (slots) {
+          const slotNames = slots.map(s => s.name);
+
+          for (const slotName of slotNames) {
+            if (!event.properties[slotName]) {
+              console.error(
+                `Event '${event.id}' of type '${event.event_type}' must have key '${slotName}' to match it's source effect component slot.`
+              );
+            }
+          }
+        } else {
+          console.error(
+            `Events of type '${event.event_type}' must have 'slots' prop.`
+          );
+        }
+
         break;
 
       default:
